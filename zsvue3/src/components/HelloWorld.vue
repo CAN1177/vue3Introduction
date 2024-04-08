41d4a56a384db0ab8bf9cdcebf19cbc84cb855c9
21Silva <21silva@21SilvadeMacBook-Pro.local>
2024-04-08 14:28:55 +0800

    feat: bem简易架构实现

---
+++ zsvue3/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+
+const arr = ["1", "2", "3"];
+</script>
+
+<template>
+  <div class="mj-test">
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+
+  <div v-for="(item, index) in arr" :key="index">{{ item }}</div>
+</template>
+
+<style scoped lang="scss">
+.read-the-docs {
+  color: #888;
+}
+@include b(test) {
+  button {
+    color: pink;
+  }
+}
+</style>
